0e991d243dd1270baef898c221c15dae663a0748
Diego Clemente <diego@pop-os.localdomain>
2023-03-29 20:24:58 -0300

    Webinar day 2

---
+++ src/components/Video.js
@@ -0,0 +1,31 @@
+import React, { useRef, useState } from "react";
+import "../styles/video.css";
+
+function Video() {
+  const videoRef = useRef(null);
+  const [play, setPlay] = useState(false);
+
+  function handlePlay() {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      setPlay(true);
+    }
+  }
+
+  return (
+    <div className="video">
+      <video
+        className="video_player"
+        ref={videoRef}
+        onClick={handlePlay}
+        src="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/brecker2.mp4?alt=media&token=b5399418-9276-4e53-a706-1e00290c2c74"
+        loop
+      ></video>
+    </div>
+  );
+}
+
+export default Video;
