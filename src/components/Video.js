bdceb22017454a7d2e5b27cb150ce9a197568f7f
Diego Clemente <diego@pop-os.localdomain>
2023-03-30 15:44:50 -0300

    Change src video

--- src/components/Video.js
+++ src/components/Video.js
@@ -21,7 +21,7 @@ function Video() {
         className="video_player"
         ref={videoRef}
         onClick={handlePlay}
-        src="https://firebasestorage.googleapis.com/v0/b/jornadadevtr.appspot.com/o/WhatsApp%20Video%202023-03-29%20at%2020.04.52.mp4?alt=media&token=db4be029-27cc-4af9-b75d-3ce5fd1b1fe2"
+        src="https://firebasestorage.googleapis.com/v0/b/jornada3-a7924.appspot.com/o/brecker2.mp4?alt=media&token=5493f40a-3b0d-4c19-b5dd-a9016cc5a6d1"
         loop
       ></video>
     </div>
