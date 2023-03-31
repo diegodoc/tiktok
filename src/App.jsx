477c1a684631d7f150cecbdf493701f8d3d3ca08
Diego Clemente <diego@pop-os.localdomain>
2023-03-30 21:44:28 -0300

    Style changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./styles/App.css";
 import "./components/Video";
 import Video from "./components/Video";
-
 function App() {
   return (
     <div className="App">
